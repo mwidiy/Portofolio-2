ae6b658c8bf79db171f65771243192e92d6cee31
Widiser <muhammadwidi72@gmail.com>
2024-10-31 08:57:30 +0700

    membuat navbar

--- src/Componens/NavbarComponen.jsx
+++ src/Componens/NavbarComponen.jsx
@@ -1,30 +1,24 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from "../assets/widiloggo.png"
 
 const NavbarComponen = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-dark navbar-dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={logo} width="50" height="30" className="d-inline-block align-top" alt="Logo" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
+        <Nav className="ms-auto">
+          <Nav.Link className="mx-3 nav-hover active" href="#home">Home</Nav.Link>
+          <Nav.Link className="mx-3 nav-hover active" href="#link">About</Nav.Link>
+          <Nav.Link className="mx-3 nav-hover active" href="#link">Projeck</Nav.Link>
+          <Nav.Link className="mx-3 nav-hover active" href="#link">Portofolio</Nav.Link>
+          <Nav.Link className="mx-3 nav-hover active" href="#link">Contak</Nav.Link>
+        </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
