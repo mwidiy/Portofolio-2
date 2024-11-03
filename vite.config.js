8fadd45a8f177579b459de53a8a940e3f1c64f4e
Widiser <muhammadwidi72@gmail.com>
2024-11-03 20:12:47 +0700

    konfigurasi pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/portofolio-2/',
+  base: '/Portofolio-2/',
   plugins: [react()],
 });
